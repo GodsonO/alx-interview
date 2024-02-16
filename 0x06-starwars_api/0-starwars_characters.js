eaee7ef2c13e3d9ba6c8e65abaee5a4a6127ccc2
GodsonO <godsonomoghene15@gmail.com>
2024-02-16 02:49:51 +0000

    star

---
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -0,0 +1,23 @@
+#!/usr/bin/node
+
+const httprequest = require('request');
+
+const movieId = process.argv[2];
+
+const url = `https://swapi-api.hbtn.io/api/films/${movieId}`;
+
+httprequest(url, async (err, res, body) => {
+  err && console.log(err);
+
+  const charactersArr = (JSON.parse(res.body).characters);
+  for (const character of charactersArr) {
+    await new Promise((resolve, reject) => {
+      httprequest(character, (err, res, body) => {
+        err && console.log(err);
+
+        console.log(JSON.parse(body).name);
+        resolve();
+      });
+    });
+  }
+});
