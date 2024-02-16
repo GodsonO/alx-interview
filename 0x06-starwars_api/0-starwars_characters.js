06db37db05c7874c5e98f321542375f0ab63670c
GodsonO <godsonomoghene15@gmail.com>
2024-02-16 02:59:11 +0000

    star

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -1,18 +1,23 @@
 #!/usr/bin/node
 
-const httpRequest = require('request');
-
-httpRequest('https://swapi-api.hbtn.io/api/films/' + process.argv[2], function (error, response, responseBody) {
-  if (error) throw error;
-  const characterUrls = JSON.parse(responseBody).characters;
-  processCharacters(characterUrls, 0);
+const request = require('request');
+const url = `https://swapi-api.hbtn.io/api/films/${process.argv[2]}/`;
+request(url, async function (error, response, body) {
+  if (error) {
+    return console.log(error);
+  } else {
+    const characters = JSON.parse(body).characters;
+    for (const character in characters) {
+      const res = await new Promise((resolve, reject) => {
+        request(characters[character], (err, res, html) => {
+          if (err) {
+            reject(err);
+          } else {
+            resolve(JSON.parse(html).name);
+          }
+        });
+      });
+      console.log(res);
+    }
+  }
 });
-
-const processCharacters = (characterUrls, index) => {
-  if (index === characterUrls.length) return;
-  httpRequest(characterUrls[index], function (error, response, responseBody) {
-    if (error) throw error;
-    console.log(JSON.parse(responseBody).name);
-    processCharacters(characterUrls, index + 1);
-  });
-};
